7c5fe217d6690fc2902b5404588f2f8fb1015666
VEDICASHUTOSH <ashutosh.singh@vedicrishiastro.com>
2026-02-11 07:20:51 +0530

    first commit

---
+++ src/concepts/basic.ts
@@ -0,0 +1,25 @@
+/*
+ * Data is stored in table format.
+ * Look similar to excel sheet.
+ * But Excel sheet has limitations.
+ * In database we can store unlimited data.
+ */
+
+/*
+ * DBMS (Database Management System) vs Database.
+ * DBMS is software that manage database.
+ * Database is collection of data.
+ * Few examples of DBMS are MySQL, PostgreSQL, MongoDB, etc.
+ */
+
+/*
+ * RDBMS (Relational Database Management System).
+ * A type of database that stores data in tables(using rows and columns) and uses SQL (structured query language) to manage data.
+ * Few examples of RDBMS are MySQL, PostgreSQL, Oracle, etc.
+ */
+
+/*
+ * SQL (Structured Query Language).
+ * A programming language used to manage and interact with databases.
+ * It is used to create, read, update, and delete data in a database.
+ */
