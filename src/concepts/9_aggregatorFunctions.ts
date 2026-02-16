c81207afeb3dd8088992a3f0ca2a0622b99fdf03
VEDICASHUTOSH <ashutosh.singh@vedicrishiastro.com>
2026-02-16 12:40:51 +0530

    aggregatore concept added

---
+++ src/concepts/9_aggregatorFunctions.ts
@@ -0,0 +1,29 @@
+/* 1. COUNT
+ * COUNT(*) returns the total number of rows in a table.
+ * COUNT(column_name) returns the number of non-NULL values in a column.
+ * COUNT(DISTINCT column_name) returns the number of unique non-NULL values in a column.
+ */
+
+/*
+ * 2. SUM
+ * SUM(column_name) returns the sum of all values in a column.
+ * SUM(DISTINCT column_name) returns the sum of unique non-NULL values in a column.
+ */
+
+/*
+ * 3. AVG
+ * AVG(column_name) returns the average of all values in a column.
+ * AVG(DISTINCT column_name) returns the average of unique non-NULL values in a column.
+ */
+
+/*
+ * 4. MAX
+ * MAX(column_name) returns the maximum value in a column.
+ * MAX(DISTINCT column_name) returns the maximum unique non-NULL value in a column.
+ */
+
+/*
+ * 5. MIN
+ * MIN(column_name) returns the minimum value in a column.
+ * MIN(DISTINCT column_name) returns the minimum unique non-NULL value in a column.
+ */
